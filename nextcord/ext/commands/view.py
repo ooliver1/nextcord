fcfb8ce07467f21a82b66c0bcc5129169f39a6c1
Oliver Wilkes <oliverwilkes2006@icloud.com>
2023-10-05 18:51:09 +0100

    refactor(commands): annotate StringView

--- nextcord/ext/commands/view.py
+++ nextcord/ext/commands/view.py
@@ -1,5 +1,7 @@
 # SPDX-License-Identifier: MIT
 
+from typing import Optional
+
 from .errors import ExpectedClosingQuoteError, InvalidEndOfQuotedStringError, UnexpectedQuoteError
 
 # map from opening quotes to closing quotes
@@ -26,24 +28,24 @@ _all_quotes = set(_quotes.keys()) | set(_quotes.values())
 
 
 class StringView:
-    def __init__(self, buffer) -> None:
-        self.index = 0
-        self.buffer = buffer
-        self.end = len(buffer)
-        self.previous = 0
+    def __init__(self, buffer: str) -> None:
+        self.index: int = 0
+        self.buffer: str = buffer
+        self.end: int = len(buffer)
+        self.previous: int = 0
 
     @property
-    def current(self):
+    def current(self) -> Optional[str]:
         return None if self.eof else self.buffer[self.index]
 
     @property
-    def eof(self):
+    def eof(self) -> bool:
         return self.index >= self.end
 
     def undo(self) -> None:
         self.index = self.previous
 
-    def skip_ws(self):
+    def skip_ws(self) -> bool:
         pos = 0
         while not self.eof:
             try:
@@ -58,7 +60,7 @@ class StringView:
         self.index += pos
         return self.previous != self.index
 
-    def skip_string(self, string) -> bool:
+    def skip_string(self, string: str) -> bool:
         strlen = len(string)
         if self.buffer[self.index : self.index + strlen] == string:
             self.previous = self.index
@@ -66,19 +68,19 @@ class StringView:
             return True
         return False
 
-    def read_rest(self):
+    def read_rest(self) -> str:
         result = self.buffer[self.index :]
         self.previous = self.index
         self.index = self.end
         return result
 
-    def read(self, n):
+    def read(self, n) -> str:
         result = self.buffer[self.index : self.index + n]
         self.previous = self.index
         self.index += n
         return result
 
-    def get(self):
+    def get(self) -> Optional[str]:
         try:
             result = self.buffer[self.index + 1]
         except IndexError:
@@ -88,7 +90,7 @@ class StringView:
         self.index += 1
         return result
 
-    def get_word(self):
+    def get_word(self) -> str:
         pos = 0
         while not self.eof:
             try:
@@ -103,7 +105,7 @@ class StringView:
         self.index += pos
         return result
 
-    def get_quoted_word(self):
+    def get_quoted_word(self) -> Optional[str]:
         current = self.current
         if current is None:
             return None
